056450b755c0dd26ffd8be5da65e2ea5b5b941f8
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-10-20 17:06:56 +0530

    i want to build things ,why i am stuck

--- first.js
+++ first.js
@@ -1,2 +1,17 @@
 const {program } = require('commander');
 
+program
+    .description('a program to repeat what you say')
+    .option('-a, --alpha', 'Alpha')
+    .option('-b, --beta,  ','nothing much','foo');
+
+
+program.parse();
+
+const options = program.opts();
+console.log('Options detected: ')
+
+if (options.alpha) console.log('alpha');
+ 
+const beta = !options.beta ? 'no' : options.beta;
+console.log('beta is: %s', beta);
