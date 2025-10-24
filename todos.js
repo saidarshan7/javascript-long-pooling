1689ff0f78f72296e59f20323db95d3943591cea
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-10-24 12:19:26 +0530

    connected to postgres database

--- todos.js
+++ todos.js
@@ -1,5 +1,40 @@
-const {Client} = require('pg');
+import {Client} from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const port = process.env.PORT;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+
+
+
+
+async function connectAndQuery() {
+  const client = new Client({
+    user: dbUser,
+    host: 'localhost',
+    database: 'todos',
+    password: dbPass,
+    port: port,
+  });
+
+
+try {
+    await client.connect();
+    console.log('Connected to PostgreSQL');
+
+
+  } catch (err) {
+    console.error('Database connection or query error', err);
+  } finally {
+    await client.end();
+    console.log('Disconnected from PostgreSQL');
+  }
+
+}
+
+connectAndQuery();
 
 const todoList = Array.from({length : 5}).map(
     (_,index) => `https://jsonplaceholder.typicode.com/todos/${++index}`
@@ -28,4 +63,5 @@ let data = async () => {
     
 } 
 
-data().then((data)=> console.log(data));
+let arr = await data();
+
